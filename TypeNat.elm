a45c79c07e0b1b1738081d226c868957a84fe40a
Joey Eremondi <joey@eremondi.com>
2015-11-12 17:52:19 -0800

    Add documentation

--- TypeNat.elm
+++ TypeNat.elm
@@ -1,17 +1,27 @@
-module List
+module TypeNat
     ( Zero, OnePlus
     ) where
 
+{-|
+Easy to remember names for type-level natural numbers.
+These are intended only to be used as "phantom types:"
+that is, there are no values of type `Zero` or `OnePlus a`.
+Instead, they should be arguments to other type constructors.
 
-type ZeroInternal = ZeroInternal ZeroInternal
+Elm doesn't have DataKinds, so please don't do anything silly like
+`OnePlus (OnePlus (One Plus (List (Int, Int))))`
+i.e. please only give another TypeNat as the argument to
+OnePlus.
+
+@docs Zero, OnePlus
+
+-}
 
 
-type OnePlusInternal a = OnePlusInternal OnePlusInternal a
+{-| A phantom type for 0, our base TypeNat -}
+type Zero = Zero Zero
 
 
-{-|A phantom type for 0 |-}
-type alias Zero = ZeroInternal
-
-
-{-|A phantom type constructor to increment a TypeNat |-}
-type alias OnePlus a = OnePlusInternal a
+{-|A phantom type constructor to increment a TypeNat,
+creating a larger TypeNat -}
+type OnePlus a = OnePlus OnePlus
